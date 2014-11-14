8fe8fbaa73a2234dc89de167aacbe291b7b51778
Amit Shah <amit.shah@woi.aero>
2014-11-14 22:35:59 +0530

    Inital startup code for A1 shoping site

---
+++ src/main/java/com/a1electronics/ecommerce/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package com.a1electronics.ecommerce.controllers;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Home Page.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>A1 electronics e-commerce site</title>
+</head>
+<body>
+<h1>
+	A1 electronics e-commerce site  
+</h1>
+<h2>Welcome customer. This site is under construction.</h2>
+</body>
+</html>
